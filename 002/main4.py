da382c86450e60509f04a6d13507d8be4ce229c1
Dasa <153184061+Dasa122@users.noreply.github.com>
2025-09-09 10:35:31 +0200

    Fix formatting in print statements for consistency in main4.py

--- 002/main4.py
+++ 002/main4.py
@@ -4,14 +4,14 @@ print(f"""
 x      =     10
 y      =     3
 ---------------------
-x + y  =     {x + y}
-x – y  =     {x - y}
-x * y  =     {x * y}
-x / y  =     {x / y}
-x // y =     {x // y} 
-x % y  =     {x % y}
-x ** 2 =     {x ** 2}
-x ** 3 =     {x ** 3}
+x + y=     {x + y}
+x – y=     {x - y}
+x * y=     {x * y}
+x / y=     {x / y}
+x // y=    {x // y} 
+x % y=     {x % y}
+x ** 2=    {x ** 2}
+x ** 3=    {x ** 3}
       """)
 
 # //  maradekot levagja int
