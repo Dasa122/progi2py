9dbe789465f7300a786d71d3c85e9e290bd7566e
Dasa <153184061+Dasa122@users.noreply.github.com>
2025-09-09 10:05:54 +0200

    Add initial project files including .gitignore, main scripts, and virtual environment configuration

---
+++ 002/main2.py
@@ -0,0 +1,15 @@
+a = 5
+b = 10
+print("a =", a, "b =", b)
+a *= 2
+print("a =", a, "b =", b)
+a += 1
+b -= 1
+print("a =", a, "b =", b)
+a = 34
+c = a + b
+print("c =", c)
+
+
+print(f"egyik szám: {a}, másik szám: {b}, összegük: {c}")
+
---
+++ 002/main3.py
@@ -0,0 +1,8 @@
+a = 1 # intrager egesz samot tarolo minket valtozo
+b = 7 # float valos szam
+c = -3 # syovegek tarolasa idozojelek kozott tortenjen
+# str string syovegek tarolasara alkalmas
+print(f"({a}-{b})/{c} = {(a-b)/c}")
+print(f"({a}+{b})*(2({a})-({c})) = {(a+b)*(2*a-(c))}")
+print(f"(3({a})-3({b}))/({c}) = {(3*a-3*b)/c}")
+print(f"2*({a})*({c})+4({b}) = {2*a*c+4*b}")
